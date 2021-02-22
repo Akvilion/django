a71c7dab58fa3f9f7685c18c5fd7d88994cc9e35
Akvilion <codeorion12@gmail.com>
2021-02-22 17:00:43 +0200

    add templates forlder + change view

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 from .models import Bb
 
 def index(request):
-    s = 'Список оголошеньй\r\n\r\n\r\n'
-    for bb in Bb.objects.order_by('-published'):
-        s+=bb.title + '\r\n' + bb.content + '\r\n\r\n'
-    return HttpResponse(s, content_type='text/plain; charset=utf-8')
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published')
+    context = {'bbs': bbs}
+    return HttpResponse(template.render(context, request))
 
